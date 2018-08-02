dd02dd853d55be06cbe66c5766b2b5dbcde84cd9
João Paulo da Silva <joao.silva@mgmcorp.com.br>
2018-08-02 13:29:13 -0300

    Adicionar arquivos de projeto.

---
+++ src/SGCliente.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGCliente.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
